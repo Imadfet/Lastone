cbb95aa6ac699d5563906884ddd5f0beb7da5baf
Imadfet <imadfettah7@gmail.com>
2023-05-04 22:38:19 +0100

    the second

--- src/main/java/com/example/lastdev/LastDevApplication.java
+++ src/main/java/com/example/lastdev/LastDevApplication.java
@@ -7,7 +7,7 @@ public class LastDevApplication {
 
     public static void main(String[] args) {
         System.out.println("it 's the first");
-        System.out.println("it 's the second");
+        System.out.println("it's the second");
     }
 
 }
