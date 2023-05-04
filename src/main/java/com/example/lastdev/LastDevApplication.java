bbeb086efc61f7f44cfd1fedb8898ecc71383f9f
Imadfet <imadfettah7@gmail.com>
2023-05-04 22:25:09 +0100

    add this first

---
+++ src/main/java/com/example/lastdev/LastDevApplication.java
@@ -0,0 +1,12 @@
+package com.example.lastdev;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastDevApplication {
+
+    public static void main(String[] args) {
+        System.out.println("it 's the first");
+    }
+
+}
---
+++ src/test/java/com/example/lastdev/LastDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lastdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
